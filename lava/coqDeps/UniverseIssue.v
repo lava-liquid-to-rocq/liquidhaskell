452c0888df0fcc04084bedb0f7e5330f9b34eee8
Colin Rothgang <colin.rothgang@imdea.org>
2026-03-25 19:04:03 +0100

    minor

--- lava/coqDeps/UniverseIssue.v
+++ lava/coqDeps/UniverseIssue.v
@@ -2,7 +2,7 @@ From coqDeps Require Export LiquidPreludeUtil.
 Open Scope Z_scope.
 Open Scope Int_scope.
 
-(* For salf-containedness's sake the definition of UArgListT is repeated below:
+(* For self-containedness's sake the definition of UArgListT is repeated below:
   Polymorphic Inductive UArgListT@{u} : Type@{u+1} :=
   | noUArgsT: UArgListT
   | consUArgsT (X:Type@{u}) (tl:UArgListT): UArgListT.
@@ -34,10 +34,10 @@ We would like to simply write:
 Definition ValF2_u'@{u}: @uPack2@{u+1} (consUArgsT (@uPack2@{u} (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z. 
 
 But Rocq doesn't allow the universe level increment syntax in that place. So instead we can try:
+*)
+Fail Definition ValF2_u'@{u}: @uPack2 (consUArgsT (@uPack2@{u} (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z. 
 
-Definition ValF2_u'@{u}: @uPack2 (consUArgsT (@uPack2@{u} (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z. 
-
-Which yields the error:
+(* Which yields the error:
 The term "(uPack2@{u} Z ::UT nilUT Z) ::UT Z ::UT nilUT" has type
  "UArgListT@{coqDeps.UniverseIssue.155}"
 while it is expected to have type "UArgListT@{UArgList.u}"
@@ -46,7 +46,6 @@ UArgList.u because UArgList.u < coqDeps.UniverseIssue.155).
 
 Apparently Rocq identifies the universe levels of the two uPacks, which is of course absolutely hopelessly wrong. 
 But as we cannot explicitely specify the correct level, I'm not sure how to address that issue.*)
-Fail Definition ValF2_u'@{u}: @uPack2@{v} (consUArgsT (@uPack2@{u} (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z. 
 
 (* The above example is a minimal faling example based on the structure on the real example below: *)
 Fail Definition ValF2_u': @uPack (consUArgsT (@uPack (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z.
