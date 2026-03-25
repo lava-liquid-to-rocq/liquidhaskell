579da697ba82385d23fe10f41e125db18aceae47
Colin Rothgang <colin.rothgang@imdea.org>
2026-03-25 19:00:38 +0100

    Create minimal example for universe issues

--- lava/coqDeps/UniverseIssue.v
+++ lava/coqDeps/UniverseIssue.v
@@ -2,10 +2,15 @@ From coqDeps Require Export LiquidPreludeUtil.
 Open Scope Z_scope.
 Open Scope Int_scope.
 
+(* For salf-containedness's sake the definition of UArgListT is repeated below:
+  Polymorphic Inductive UArgListT@{u} : Type@{u+1} :=
+  | noUArgsT: UArgListT
+  | consUArgsT (X:Type@{u}) (tl:UArgListT): UArgListT.
+*)
+
 Polymorphic Definition uPack2@{u} (A:UArgListT@{u}) (B:Type@{u}): Type@{u+1} := 
   UArgList A -> B.
 
-Check consUArgsT.
 (* We want to define something like this:
 Definition inter@{u} : UArgListT@{u+1} := consUArgsT (@uPack2@{u} (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT).
 
@@ -16,7 +21,7 @@ If instead we omit the explicit universe levels, as below, Rocq creates a new fr
 
 Definition inter : UArgListT := consUArgsT (@uPack2 (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT).
 Print inter.
-(* Output:
+(* yields
 inter =
 (uPack2@{UArgList.u} Z ::UT nilUT Z) ::UT Z ::UT nilUT
      : UArgListT@{inter.u0}
@@ -41,4 +46,10 @@ UArgList.u because UArgList.u < coqDeps.UniverseIssue.155).
 
 Apparently Rocq identifies the universe levels of the two uPacks, which is of course absolutely hopelessly wrong. 
 But as we cannot explicitely specify the correct level, I'm not sure how to address that issue.*)
-Definition ValF2_u'@{u}: @uPack2 (consUArgsT (@uPack2@{u} (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z. 
+Fail Definition ValF2_u'@{u}: @uPack2@{v} (consUArgsT (@uPack2@{u} (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z. 
+
+(* The above example is a minimal faling example based on the structure on the real example below: *)
+Fail Definition ValF2_u': @uPack (consUArgsT (@uPack (consUArgsT Z nilUT) Z) (consUArgsT Z nilUT)) Z -> Z.
+(* which in turn models the issue in the translation of higher-order datatypes of rank >2, exemplified below: *) 
+Fail Inductive Identity_F2 : Type :=
+  ValF2_u': @uPack2 ((@uPack2 (Z ::UT nilUT) Z) ::UT (Z ::UT nilUT)) Z -> Z.
