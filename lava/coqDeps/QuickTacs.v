468cfb7e811c7c65b5311c8103ee16a8f1dc6ffe
Colin Rothgang <colin.rothgang@imdea.org>
2026-04-23 23:05:16 +0200

    minor optimization

--- lava/coqDeps/QuickTacs.v
+++ lava/coqDeps/QuickTacs.v
@@ -446,6 +446,7 @@ Ltac shape_based := match goal with
         assert (d -> False) as temp by (intro; repeat shape_based; fast_done); left];
       clear temp
   (*| |- exists v, ?relAp v => solve [unshelve (eexists _; econstructor; assumption)]*)
+  | |- exists v, ?relAp v /\ v = ?t => exists t; split; [|reflexivity]
   | |- _ => intro
   end.
 
